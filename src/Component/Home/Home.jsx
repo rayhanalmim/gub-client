1cb34aa71656a5c2efcb672dfb3d544a51f14b17
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-14 16:43:05 +0600

    change navlink color

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,74 +1,73 @@
-import { Link, Outlet, } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { FaChartBar, FaBeer, FaUser, FaShoppingCart, FaListUl } from "react-icons/fa";
 import { FaShop } from "react-icons/fa6";
 
 const Home = () => {
     return (
-        <div className=" min-h-screen">
-            <div className="flex flex-col lg:flex-row w-full  mx-auto  relative">
-                <div className="w-full lg:w-1/4  hidden lg:block">
-                    <div className="justify-around  top-6 sticky p-6 lg:p-8 gap-9 text-center   w-full mx-auto ">
+        <div className="min-h-screen">
+            <div className="flex flex-col lg:flex-row w-full mx-auto relative">
+                <div className="w-full lg:w-1/4 hidden lg:block">
+                    <div className="justify-around top-6 sticky p-6 lg:p-8 gap-9 text-center w-full mx-auto">
                         <div className="flex flex-col gap-4">
 
                             <div className="flex justify-center py-5 items-center border-b-2 border-black">
                                 <h3 className="text-3xl "><span className="font-bold">Teacher</span> <br /> Dashboard</h3>
                             </div>
 
-                            <Link to="/">
-                                <button className="btn bg-sky-800  hover:text-black text-white w-full ">
+                            <NavLink to="/" activeClassName="active">
+                                <button className="btn bg-sky-800 hover:text-black text-white w-full">
                                     <FaShoppingCart className="mr-2" />
                                     Home
                                 </button>
-                            </Link>
+                            </NavLink>
 
-                            <Link to="/student">
-                                <button className="btn bg-sky-800  hover:text-black text-white w-full ">
+                            <NavLink to="/student" activeClassName="active">
+                                <button className="btn bg-sky-800 hover:text-black text-white w-full">
                                     <FaShoppingCart className="mr-2" />
                                     DSR Order Request
                                 </button>
-                            </Link>
+                            </NavLink>
 
-                            <Link to="/sellView">
-                                <li className="btn bg-sky-800 text-white w-full">
+                            <NavLink to="/sellView" activeClassName="active">
+                                <button className="btn bg-sky-800 text-white w-full">
                                     <FaListUl className="mr-2" />
                                     Billing Management
-                                </li>
-                            </Link>
+                                </button>
+                            </NavLink>
 
-                            <Link to="/manageProduct">
-                                <li className="btn bg-sky-800 hover:text-black text-white w-full">
+                            <NavLink to="/manageProduct" activeClassName="active">
+                                <button className="btn bg-sky-800 hover:text-black text-white w-full">
                                     <FaBeer className="mr-2" />
                                     Product Management
-                                </li>
-                            </Link>
+                                </button>
+                            </NavLink>
 
-                            <Link to="/shop">
-                                <li className="btn bg-sky-800 hover:text-black text-white w-full">
+                            <NavLink to="/shop" activeClassName="active">
+                                <button className="btn bg-sky-800 hover:text-black text-white w-full">
                                     <FaShop className="mr-2" />
                                     Client Shop Management
-                                </li>
-                            </Link>
+                                </button>
+                            </NavLink>
 
-                            <Link to="/user">
-                                <li className="btn bg-sky-800 hover:text-black text-white w-full">
+                            <NavLink to="/user" activeClassName="active">
+                                <button className="btn bg-sky-800 hover:text-black text-white w-full">
                                     <FaUser className="mr-2" />
                                     Employee Management
-                                </li>
-                            </Link>
+                                </button>
+                            </NavLink>
 
-                            <Link to="/analysis">
-                                <li className="btn bg-sky-800 hover:text-black text-white w-full">
+                            <NavLink to="/analysis" activeClassName="active">
+                                <button className="btn bg-sky-800 hover:text-black text-white w-full">
                                     <FaChartBar className="mr-2" />
                                     Sell Analysis
-                                </li>
-                            </Link>
-
+                                </button>
+                            </NavLink>
 
                         </div>
                     </div>
                 </div>
                 <div className="w-full lg:w-3/4 pt-5 lg:pt-32 sticky top-10">
-                    <div className="w-full justify-around lg:p-8 gap-9 text-center lg:-mt-32 bg-slate-100 ">
+                    <div className="w-full justify-around lg:p-8 gap-9 text-center lg:-mt-32 bg-slate-100">
                         <div className="pb-10">
                             <h3>navbar comming</h3>
                         </div>
