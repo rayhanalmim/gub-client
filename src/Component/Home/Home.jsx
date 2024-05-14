1423267a20a26c2ce73f9913d23226b97515478f
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-14 17:01:14 +0600

    fixed layout with color of navbar

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,12 +1,13 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { FaChartBar, FaBeer, FaUser, FaShoppingCart, FaListUl } from "react-icons/fa";
 import { FaShop } from "react-icons/fa6";
+import NavBar from "../shared/NavBar";
 
 const Home = () => {
     return (
         <div className="min-h-screen">
-            <div className="flex flex-col lg:flex-row w-full mx-auto relative">
-                <div className="w-full lg:w-1/4 hidden lg:block">
+            <div className="flex flex-col lg:flex-row bg-slate-100 w-full mx-auto relative">
+                <div className="w-full lg:w-1/4 hidden bg-white lg:block">
                     <div className="justify-around top-6 sticky p-6 lg:p-8 gap-9 text-center w-full mx-auto">
                         <div className="flex flex-col gap-4">
 
@@ -66,11 +67,11 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
-                <div className="w-full lg:w-3/4 pt-5 lg:pt-32 sticky top-10">
+                <div >
+                <NavBar></NavBar>
+                </div>
+                <div className="w-full lg:w-3/4 pt-5 lg:pt-52 sticky top-10">
                     <div className="w-full justify-around lg:p-8 gap-9 text-center lg:-mt-32 bg-slate-100">
-                        <div className="pb-10">
-                            <h3>navbar comming</h3>
-                        </div>
                         <Outlet />
                     </div>
                 </div>
---
+++ src/Component/shared/NavBar.jsx
@@ -0,0 +1,89 @@
+import  { useState, useEffect, useRef } from 'react';
+// import './nav.css'; // Import your CSS file
+import {  Link } from 'react-router-dom';
+import { FaTimes } from 'react-icons/fa';
+import { AiOutlineMenuUnfold } from 'react-icons/ai';
+
+// Create a React component with state variables for scroll-based color changes
+const NavBar = () => {
+  const [navbarColor, setNavbarColor] = useState('bg-transparent');
+  const [navbarTextColor, setNavbarTextColor] = useState('text-black');
+  const [navbarShadow, setNavbarShadow] = useState('');
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const drawerRef = useRef(null);
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setNavbarColor('bg-sky-800');
+        setNavbarTextColor('text-white');
+        setNavbarShadow('shadow');
+      } else {
+        setNavbarColor('bg-transparent');
+        setNavbarTextColor('text-black');
+        setNavbarShadow('');
+      }
+    };
+
+    const handleClickOutside = (event) => {
+      if (drawerRef.current && !drawerRef.current.contains(event.target)) {
+        setIsDrawerOpen(false);
+      }
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+ 
+
+  return (
+    <div>
+      <div className={` navbar fixed ${navbarColor} bg-opacity-75 px-1 md:px-10 z-20 transition-colors duration-300 ${navbarShadow}`}>
+        <div className={`navbar-start ${navbarTextColor}`}>
+          {/* Drawer for mobile view */}
+          <button onClick={toggleDrawer} className="btn btn-ghost lg:hidden">
+            <AiOutlineMenuUnfold className={`h-5 w-5 ${navbarTextColor}`} />
+          </button>
+          <div
+            ref={drawerRef}
+            className={`fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform ${isDrawerOpen ? '' : '-translate-x-full'} bg-cyan-700 w-64`}
+          >
+            <button onClick={toggleDrawer} className="btn btn-ghost">
+              <FaTimes className={`h-6 w-6 ${navbarTextColor}`} />
+            </button>
+          </div>
+          <div className=' flex gap-2'>
+            <div className='flex justify-center items-center'>
+              <div className='h-10 w-10 md:h-14 bg-white rounded-full md:w-14 '>
+                <img src="https://i.ibb.co/kyg59Y0/pngegg.png" alt="" />
+              </div>
+            </div>
+            <div className='space-y-0 pt-0 md:pt-1 w-60'>
+              <h3 className={`font-semibold ${navbarTextColor}`}>HealthCare</h3>
+              <p className='leading-none w-full'>Private Limited</p>
+            </div>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex"></div>
+      
+      </div>
+    </div>
+  );
+};
+
+export default NavBar; 
