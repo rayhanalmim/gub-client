dcb78254d7860edf8e4da6fb4d0f378a0fe7fcb3
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-27 20:11:10 +0600

    created card for display course

--- src/Route/Router.jsx
+++ src/Route/Router.jsx
@@ -3,35 +3,51 @@ import Root from "./Root";
 import Table from "../Component/OtherComponent/Table";
 import SignIn from "../Component/Authentication/SignIn/SignIn";
 import AdminRoute from "../Component/PrivateRoute/AdminRoute";
+import Course from "../Component/course/Course";
 
 const Router = createBrowserRouter([
-    {
-        path:'/',
-        element:<Root></Root>,
-        children: [
-            {
-                path: '/',
-                element: <AdminRoute><h3>hwllo</h3></AdminRoute>
-            },
-            {
-                path: '/coursedetails',
-                element: <AdminRoute><h3>hwllo</h3></AdminRoute>
-            },
-            {
-                path: '/launch',
-                element: <AdminRoute><Table></Table></AdminRoute>
-            },
-            {
-                path: '/attendance',
-                element: <AdminRoute><h3>hwllo</h3></AdminRoute>
-            },
-           
-        ]
-    },
-    {
-        path: '/signIn',
-        element: <SignIn></SignIn>
-    },
-])
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: (
+          <AdminRoute>
+            <h3>hwllo</h3>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/coursedetails",
+        element: (
+          <AdminRoute>
+            <Course></Course>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/launch",
+        element: (
+          <AdminRoute>
+            <Table></Table>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/attendance",
+        element: (
+          <AdminRoute>
+            <h3>hwllo</h3>
+          </AdminRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "/signIn",
+    element: <SignIn></SignIn>,
+  },
+]);
 
 export default Router;
