8c82bd2788c6aca7343f8e03db395f97429c7874
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-28 00:16:38 +0600

    created course delete functionality

--- src/Component/course/Course.jsx
+++ src/Component/course/Course.jsx
@@ -1,8 +1,42 @@
 import { Link } from "react-router-dom";
+import { RiChatDeleteFill } from "react-icons/ri";
 import useCourse from "../../Hook/useCourse";
+import Swal from "sweetalert2";
+import useAxiosPublic from "../../Hook/useAxiosPublic";
 
 const Course = () => {
-  const { course, isLoading, refetch } = useCourse();
+  const { course, refetch } = useCourse();
+  const axiosPublic = useAxiosPublic();
+
+  const hendleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosPublic.delete(`/deleteCourse?id=${id}`);
+        if (res.status == 200) {
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success",
+          });
+          refetch();
+        } else {
+          Swal.fire({
+            title: "something went wrong!",
+            text: "Your file has not been deleted.",
+            icon: "error",
+          });
+        }
+      }
+    });
+  };
 
   return (
     <div className="grid grid-cols-3 gap-3">
@@ -18,6 +52,12 @@ const Course = () => {
           <h2 className="text-3xl font-extrabold pb-1 tracking-widest text-white">
             {cour?.courseName}
           </h2>
+          <div
+            onClick={() => hendleDelete(cour?._id)}
+            className="absolute top-2 right-2 cursor-pointer"
+          >
+            <RiChatDeleteFill className="text-2xl"></RiChatDeleteFill>
+          </div>
           <p className="text-white dark:text-gray-400"> {cour?.courseCode}</p>
 
           <div className="flex justify-between">
