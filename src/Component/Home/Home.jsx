27efc301ef083f83f03304b7cddb79bee593bf16
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-27 16:36:40 +0600

    fixed navbar

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -5,102 +5,113 @@ import { AiOutlineEdit } from "react-icons/ai";
 import NavBar from "../shared/NavBar";
 
 const Home = () => {
+  const location = useLocation(); // Get the current location using useLocation
 
-    const location = useLocation(); // Get the current location using useLocation
-
-
-    return (
-        <div className=" ">
-
-            <div className="flex flex-col lg:flex-row w-full bg-[url('https://i.ibb.co/M8X9LN5/iconic-photo-of-green-university-dp-20210309132337.jpg')] bg-cover bg-center h-3/4 md:h-full  inset-0 bg-black bg-opacity-55 bg-blend-multiply "> {/* bg img sec */}
-
-                <div className="w-full  lg:w-1/4 bg-opacity-30 bg-black transition-colors duration-300  hidden lg:block">
-
-                    <div className="justify-around fixed z-20 top-4 p-6 lg:p-8 gap-9 text-center w-1/4 mx-auto">
-
-                        <div className='h-32 w-32 bg-white mx-auto rounded-full '>
-                            <img src="https://i.ibb.co/xMbQDNh/Green-University-of-Bangladesh-logo-svg.png" alt="" />
-                        </div>
-                        <div className='space-y-0 pt-0 mx-auto md:pt-1 w-60'>
-                            <h3 className={`font-bold text-gray-50 text-3xl leading-none pb-2 `}>Green University Of Bangladesh</h3>
-                            <p className='leading-none w-full text-gray-300 font-extrabold tracking-wide'>Faculty Dashboard</p>
-                        </div>
-                        <div className="flex flex-col border-t-2  border-gray-200 mt-5 pt-5 gap-4">
-
-                            {/* <div className="flex justify-center py-5 items-center border-b-2 border-black">
+  return (
+    <div className=" ">
+      <div className="flex flex-col lg:flex-row w-full bg-[url('https://i.ibb.co/M8X9LN5/iconic-photo-of-green-university-dp-20210309132337.jpg')] bg-cover bg-center h-3/4 md:h-full  inset-0 bg-black bg-opacity-55 bg-blend-multiply ">
+        {" "}
+        {/* bg img sec */}
+        <div className="w-full  lg:w-1/4 bg-opacity-30 bg-black transition-colors duration-300  hidden lg:block">
+          <div className="justify-around fixed z-20 top-4 p-6 lg:p-8 gap-9 text-center w-1/4 mx-auto">
+            <div className="h-32 w-32 bg-white mx-auto rounded-full ">
+              <img
+                src="https://i.ibb.co/xMbQDNh/Green-University-of-Bangladesh-logo-svg.png"
+                alt=""
+              />
+            </div>
+            <div className="space-y-0 pt-0 mx-auto md:pt-1 w-60">
+              <h3
+                className={`font-bold text-gray-50 text-3xl leading-none pb-2 `}
+              >
+                Green University Of Bangladesh
+              </h3>
+              <p className="leading-none w-full text-gray-300 font-extrabold tracking-wide">
+                Faculty Dashboard
+              </p>
+            </div>
+            <div className="flex flex-col border-t-2  border-gray-200 mt-5 pt-5 gap-4">
+              {/* <div className="flex justify-center py-5 items-center border-b-2 border-black">
                                 <h3 className="text-3xl "><span className="font-bold">Teacher</span> <br /> Dashboard</h3>
                             </div> */}
 
-                            {
-                                location.pathname === '/' ? <NavLink to="/" activeClassName="active">
-                                <button className="btn  hover:text-black hover:bg-teal-300 bg-teal-300 text-black w-full">
-                                    <FaHome className="mr-2 text-xl" />
-                                    Home
-                                </button>
-                            </NavLink> : <NavLink to="/" activeClassName="active">
-                                <button className="btn bg-emerald-800 hover:text-black hover:bg-teal-300 text-white w-full">
-                                    <FaHome className="mr-2 text-xl" />
-                                    Home
-                                </button>
-                            </NavLink>
-                            }
+              {location.pathname === "/" ? (
+                <NavLink to="/" activeClassName="active">
+                  <button className="btn  hover:text-black hover:bg-teal-300 bg-teal-300 text-black w-full">
+                    <FaHome className="mr-2 text-xl" />
+                    Home
+                  </button>
+                </NavLink>
+              ) : (
+                <NavLink to="/" activeClassName="active">
+                  <button className="btn bg-emerald-800 hover:text-black hover:bg-teal-300 text-white w-full">
+                    <FaHome className="mr-2 text-xl" />
+                    Home
+                  </button>
+                </NavLink>
+              )}
 
-                            {
-                                location.pathname === '/launch' ? <NavLink to="/launch" activeClassName="active">
-                                    <button className="btn hover:text-black hover:bg-teal-300 bg-teal-300 text-black w-full">
-                                        <MdOutlineNotStarted className="mr-2 text-2xl" />
-                                        Launch Course
-                                    </button>
-                                </NavLink> : <NavLink to="/launch" activeClassName="active">
-                                    <button className="btn bg-emerald-800 hover:text-black hover:bg-teal-300 text-white w-full">
-                                        <MdOutlineNotStarted className="mr-2 text-2xl" />
-                                        Launch Course
-                                    </button>
-                                </NavLink>
-                            }
+              {location.pathname === "/launch" ? (
+                <NavLink to="/launch" activeClassName="active">
+                  <button className="btn hover:text-black hover:bg-teal-300 bg-teal-300 text-black w-full">
+                    <MdOutlineNotStarted className="mr-2 text-2xl" />
+                    Launch Course
+                  </button>
+                </NavLink>
+              ) : (
+                <NavLink to="/launch" activeClassName="active">
+                  <button className="btn bg-emerald-800 hover:text-black hover:bg-teal-300 text-white w-full">
+                    <MdOutlineNotStarted className="mr-2 text-2xl" />
+                    Launch Course
+                  </button>
+                </NavLink>
+              )}
 
-                            {
-                                location.pathname === '/coursedetails' ? <NavLink to="/coursedetails" activeClassName="active">
-                                    <button className="btn bg-teal-300 text-black hover:text-black hover:bg-teal-300  w-full">
-                                        <FaBookReader className="mr-2 text-xl" />
-                                        Existing Course
-                                    </button>
-                                </NavLink> : <NavLink to="/coursedetails" activeClassName="active">
-                                    <button className="btn bg-emerald-800 hover:text-black hover:bg-teal-300 text-white w-full">
-                                        <FaBookReader className="mr-2 text-xl" />
-                                        Existing Course
-                                    </button>
-                                </NavLink>
-                            }
+              {location.pathname === "/coursedetails" ? (
+                <NavLink to="/coursedetails" activeClassName="active">
+                  <button className="btn bg-teal-300 text-black hover:text-black hover:bg-teal-300  w-full">
+                    <FaBookReader className="mr-2 text-xl" />
+                    Existing Course
+                  </button>
+                </NavLink>
+              ) : (
+                <NavLink to="/coursedetails" activeClassName="active">
+                  <button className="btn bg-emerald-800 hover:text-black hover:bg-teal-300 text-white w-full">
+                    <FaBookReader className="mr-2 text-xl" />
+                    Existing Course
+                  </button>
+                </NavLink>
+              )}
 
-                            {
-                                location.pathname === '/attendance' ?  <NavLink to="/attendance" activeClassName="active">
-                                <button className="btn hover:text-black hover:bg-teal-300 bg-teal-300 text-black w-full">
-                                    <AiOutlineEdit className="mr-2 text-xl" />
-                                    Attendance Portal
-                                </button>
-                            </NavLink> :  <NavLink to="/attendance" activeClassName="active">
-                                <button className="btn bg-emerald-800 hover:text-black hover:bg-teal-300 text-white w-full">
-                                    <AiOutlineEdit className="mr-2 text-xl" />
-                                    Attendance Portal
-                                </button>
-                            </NavLink>
-                            }
-
-                        </div>
-                    </div>
-                </div>
-                <div >
-                    <NavBar></NavBar>
-                </div>
-                <div className="w-full min-h-screen lg:w-3/4 pt-5 lg:pt-[220px] rounded-tl-lg top-10">
-                    <div className="w-full justify-around lg:p-8 gap-9 text-center lg:-mt-32  ">
-                        <Outlet />
-                    </div>
-                </div>
+              {location.pathname === "/attendance" ? (
+                <NavLink to="/attendance" activeClassName="active">
+                  <button className="btn hover:text-black hover:bg-teal-300 bg-teal-300 text-black w-full">
+                    <AiOutlineEdit className="mr-2 text-xl" />
+                    Attendance Portal
+                  </button>
+                </NavLink>
+              ) : (
+                <NavLink to="/attendance" activeClassName="active">
+                  <button className="btn bg-emerald-800 hover:text-black hover:bg-teal-300 text-white w-full">
+                    <AiOutlineEdit className="mr-2 text-xl" />
+                    Attendance Portal
+                  </button>
+                </NavLink>
+              )}
             </div>
+          </div>
         </div>
-    );
+        <div className="">
+          <NavBar></NavBar>
+        </div>
+        <div className="w-full min-h-screen lg:w-3/4 pt-5 lg:pt-[220px] rounded-tl-lg top-10">
+          <div className="w-full justify-around lg:p-8 gap-9 text-center lg:-mt-32  ">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/Component/shared/NavBar.jsx
+++ src/Component/shared/NavBar.jsx
@@ -1,16 +1,16 @@
-import { useState, useEffect, useRef } from 'react';
-import { FaTimes } from 'react-icons/fa';
-import { AiOutlineMenuUnfold } from 'react-icons/ai';
-import { Link, useLocation } from 'react-router-dom'; // Import useLocation from react-router-dom
-import useIsLogin from '../../Hook/useIsLogin';
-import useAxiosPublic from '../../Hook/useAxiosPublic';
-import Swal from 'sweetalert2';
+import { useState, useEffect, useRef } from "react";
+import { FaTimes } from "react-icons/fa";
+import { AiOutlineMenuUnfold } from "react-icons/ai";
+import { Link, useLocation } from "react-router-dom"; // Import useLocation from react-router-dom
+import useIsLogin from "../../Hook/useIsLogin";
+import useAxiosPublic from "../../Hook/useAxiosPublic";
+import Swal from "sweetalert2";
 
 const NavBar = () => {
   const axiosPublic = useAxiosPublic();
-  const [navbarColor, setNavbarColor] = useState('bg-green-800');
-  const [navbarTextColor, setNavbarTextColor] = useState('text-white');
-  const [navbarShadow, setNavbarShadow] = useState('');
+  const [navbarColor, setNavbarColor] = useState("bg-green-800");
+  const [navbarTextColor, setNavbarTextColor] = useState("text-white");
+  const [navbarShadow, setNavbarShadow] = useState("");
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const drawerRef = useRef(null);
   const location = useLocation(); // Get the current location using useLocation
@@ -20,13 +20,13 @@ const NavBar = () => {
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 50) {
-        setNavbarColor('bg-emerald-300');
-        setNavbarTextColor(' text-black');
-        setNavbarShadow('shadow');
+        setNavbarColor("bg-emerald-300");
+        setNavbarTextColor(" text-black");
+        setNavbarShadow("shadow");
       } else {
-        setNavbarColor(' bg-green-800  ');
-        setNavbarTextColor('text-white');
-        setNavbarShadow('');
+        setNavbarColor(" bg-green-800  ");
+        setNavbarTextColor("text-white");
+        setNavbarShadow("");
       }
     };
 
@@ -36,12 +36,12 @@ const NavBar = () => {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
-    document.addEventListener('mousedown', handleClickOutside);
+    window.addEventListener("scroll", handleScroll);
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-      document.removeEventListener('mousedown', handleClickOutside);
+      window.removeEventListener("scroll", handleScroll);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
@@ -49,80 +49,83 @@ const NavBar = () => {
     setIsDrawerOpen(!isDrawerOpen);
   };
 
-  const handleLogOut = async() => {
-    const res = await axiosPublic.get('/getLogOut');
-    if(res.data){
+  const handleLogOut = async () => {
+    const res = await axiosPublic.get("/getLogOut");
+    if (res.data) {
       Swal.fire({
         icon: "success",
         title: "Success...",
         text: "Sign Out successfully",
-    });
-    refetch();
+      });
+      refetch();
     }
-
-  }
+  };
 
   return (
-    <div className='flex justify-between'>
-      <div className={` navbar fixed py-5 ${navbarColor}  bg-opacity-75 px-1 md:px-10 z-20 transition-colors duration-300 ${navbarShadow}`}>
-        <div className={`navbar-start ${navbarTextColor}`}>
+    <div className="">
+      <div
+        className={` navbar fixed flex md:gap-10 lg:gap-[280px] xl:gap-[400px] 2xl:gap-[550px] py-5 ${navbarColor}  bg-opacity-75 px-1 md:px-10 z-20 transition-colors duration-300 ${navbarShadow}`}
+      >
+        <div className={` ${navbarTextColor}`}>
           {/* Drawer for mobile view */}
           <button onClick={toggleDrawer} className="btn btn-ghost lg:hidden">
             <AiOutlineMenuUnfold className={`h-5 w-5 ${navbarTextColor}`} />
           </button>
           <div
             ref={drawerRef}
-            className={`fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform ${isDrawerOpen ? '' : '-translate-x-full'} bg-cyan-700 w-64`}
+            className={`fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform ${
+              isDrawerOpen ? "" : "-translate-x-full"
+            } bg-cyan-700 w-64`}
           >
             <button onClick={toggleDrawer} className="btn btn-ghost">
               <FaTimes className={`h-6 w-6 ${navbarTextColor}`} />
             </button>
           </div>
-          <div className=''>
-            <div className=''>
-              {
-                location.pathname === '/' && <h3>Pages / Home </h3>
-              }
-              {
-                location.pathname === '/launch' && <h3>Pages / launch Course </h3>
-              }
-              {
-                location.pathname === '/coursedetails' && <h3>Pages / Course Details </h3>
-              }
-              {
-                location.pathname === '/attendance' && <h3>Pages / Attendance Portal </h3>
-              }
+          <div className="">
+            <div className="">
+              {location.pathname === "/" && <h3>Pages / Home </h3>}
+              {location.pathname === "/launch" && (
+                <h3>Pages / launch Course </h3>
+              )}
+              {location.pathname === "/coursedetails" && (
+                <h3>Pages / Course Details </h3>
+              )}
+              {location.pathname === "/attendance" && (
+                <h3>Pages / Attendance Portal </h3>
+              )}
             </div>
 
             <div>
-              {
-                location.pathname === '/' && <h3 className='text-2xl tracking-wide  font-bold'>
+              {location.pathname === "/" && (
+                <h3 className="text-2xl tracking-wide  font-bold">
                   Faculty Home
                 </h3>
-              }
-              {
-                location.pathname === '/coursedetails' && <h3 className='text-2xl tracking-wide font-bold'>
+              )}
+              {location.pathname === "/coursedetails" && (
+                <h3 className="text-2xl tracking-wide font-bold">
                   Course Details
                 </h3>
-              }
-              {
-                location.pathname === '/launch' && <h3 className='text-2xl tracking-wide  font-bold'>
+              )}
+              {location.pathname === "/launch" && (
+                <h3 className="text-2xl tracking-wide  font-bold">
                   launch Course
                 </h3>
-              }
-              {
-                location.pathname === '/attendance' && <h3 className='text-2xl tracking-wide  font-bold'>
+              )}
+              {location.pathname === "/attendance" && (
+                <h3 className="text-2xl tracking-wide  font-bold">
                   Attendance Portal
                 </h3>
-              }
-
+              )}
             </div>
-
           </div>
         </div>
         <div className="">
           <div className="">
-            <h3 className={`btn btn-ghost normal-case text-xl font-bold ${navbarTextColor}`}>Rayhan Al Mim</h3>
+            <h3
+              className={`btn btn-ghost normal-case text-xl font-bold ${navbarTextColor}`}
+            >
+              Rayhan Al Mim
+            </h3>
           </div>
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar mr-2">
             <div className="w-10 rounded-full">
@@ -130,12 +133,16 @@ const NavBar = () => {
             </div>
           </label>
           <div>
-            <Link to='/signIn' onClick={handleLogOut} className={`btn w-28 btn-outline ${navbarTextColor} btn-sm }`}>Log Out</Link>
+            <Link
+              to="/signIn"
+              onClick={handleLogOut}
+              className={`btn w-28 btn-outline ${navbarTextColor} btn-sm }`}
+            >
+              Log Out
+            </Link>
           </div>
         </div>
       </div>
-
-
     </div>
   );
 };
